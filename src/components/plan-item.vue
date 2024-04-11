fffb871f4fa88297e152f8a901e8f0574e4f0123
Yahir Luciano Atilano Sosa <yahiratilano7@gmail.com>
2024-04-11 11:47:05 -0600

    ⭐First Component

---
+++ src/components/plan-item.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="plan">
+        <div class="description">
+          <span class="title">
+            El Soltero
+          </span>
+        </div>
+      </div>
+
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
